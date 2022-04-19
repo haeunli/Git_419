7cdcaa915169dcde7d851b2212eff4ab9a5e63c6
USER <lynn60531@gmail.com>
2022-04-19 17:08:06 +0900

    Initial Commit

---
+++ Git_419/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Git_419 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Git_419.settings')
+
+application = get_asgi_application()
